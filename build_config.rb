4ac92784dd3849b23d7eb60886bf39bad77dbfee
Yuki Koya(zipper) <ykky@paperboy.co.jp>
2016-04-06 11:58:49 +0900

    add mrbgems

--- build_config.rb
+++ build_config.rb
@@ -1,5 +1,25 @@
 def gem_config(conf)
-  #conf.gembox 'default'
+  conf.gembox 'full-core'
+
+  conf.gem :github => 'iij/mruby-io'
+  conf.gem :github => 'iij/mruby-env'
+  conf.gem :github => 'iij/mruby-dir'
+  conf.gem :github => 'iij/mruby-digest'
+  conf.gem :github => 'iij/mruby-process'
+  conf.gem :github => 'iij/mruby-pack'
+  conf.gem :github => 'iij/mruby-socket'
+  conf.gem :github => 'mattn/mruby-json'
+  conf.gem :github => 'mattn/mruby-onig-regexp'
+  conf.gem :github => 'matsumoto-r/mruby-redis'
+  # conf.gem :github => 'matsumoto-r/mruby-memcached'
+  conf.gem :github => 'matsumoto-r/mruby-sleep'
+  conf.gem :github => 'matsumoto-r/mruby-userdata'
+  conf.gem :github => 'matsumoto-r/mruby-uname'
+  conf.gem :github => 'matsumoto-r/mruby-mutex'
+  conf.gem :github => 'matsumoto-r/mruby-localmemcache'
+
+  # add Test module
+  conf.gem :github => 'iij/mruby-mtest'
 
   # be sure to include this gem (the cli app)
   conf.gem File.expand_path(File.dirname(__FILE__))
